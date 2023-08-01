8c8f1292fb9994f17ad7d829f91473660143f136
hacker X <141136594+hackerstech@users.noreply.github.com>
2023-08-01 04:48:01 -0700

    Add files via upload

---
+++ increaser.sh
@@ -0,0 +1,14 @@
+cat banner.txt | lolcat -p .6
+
+
+echo -e "\e[0;33m This trick consist of following & Unfollowing celegrams id"
+echo -e "special thanks to t.me/nirvan_marg \e[0m"
+    			 
+
+			
+read -p "Enter Username : " U
+		         	
+read -p "Enter password :" -s S
+		 	 	
+python3 ins.py "$U" "$S"
+
---
+++ update.sh
@@ -0,0 +1,5 @@
+cd .. 
+rm -rf followers-increaser
+git clone https://github.com/security-softwares/followers-increaser/
+cd followers-increaser
+bash increaser.sh
